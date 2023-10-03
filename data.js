2859d17ce2fa220fabb031aca15a56be8eeda6e9
Apple <apple@192.168.1.4>
2023-10-03 23:57:59 +0530

    how to node js work,create server,create api,make html page,asynchoronous method.

---
+++ data.js
@@ -0,0 +1,5 @@
+const data=[{name:'meera' ,email:'meera@gmail.com'},
+{name:'meera' ,email:'meera@gmail.com'},
+{name:'mitansh' ,email:'mitansh@gmail.com'},
+{name:'seema' ,email:'seema@gmail.com'}];
+module.exports=data;
--- index.js
+++ index.js
@@ -1,26 +1,120 @@
-const app = require('./app')
-//console.log(app);
+  //json package
+const colors=require('colors');
+console.log("package.json".green);
+
+//to start nodemon
+
+console.warn("try nodemon with node js");
+console.log("code step by step"); 
+
+//   to create api
+
+const http =require('http');
+const data =require('./data');
+http.createServer((req,res)=>{
+
+  res.writeHead(200,{'content-Type':'application\json'});
+  res.write(JSON.stringify(data));
+  res.end();
+}).listen(5000);
 
 
-//var x = 20;
-const arr=[2,3,4,5,6,7,3,3,9];
+//make files in a folder
 
-  //  console.log(arr[1]);
- let result= arr.filter((item)=>{
-   return item===3
+const fs = require('fs');
+const path = require('path');
+const dirPath= path.join(__dirname,'files');
 
+for(i=0;i<5;i++)
+{
+  fs.writeFileSync(dirPath+"/hello"+i+".txt","A simple text file");
+}
+fs.readdir(dirPath,(err,files)=>{
+ files.forEach((item)=>{
+    console.log("file name is",item);
+  })//
+  console.warn(files);
+})
+
+//crud with file system
+
+
+
+const dirpath=path.join(__dirname,'crud');
+const filepath=`${dirpath}/apple.txt`;
+//  fs.writeFileSync(filepath,'this is simple text file');
+  
+fs.readFile(filepath,'utf8',(err,item)=>{
+ console.log(item);
+})
+
+//fs.appendFile(filepath,'  and file name is apple.txt',(err)=>{
+// if(!err) console.log("file is updated");
+//})
+
+// asynchronous method
+
+console.log("start exe......");
+
+setTimeout(()=>{
+  console.log("logic exe....");
+},2000)
+
+console.log("complete exe...");
+
+let a=10;
+let b=0;
+
+
+setTimeout(()=>{
+  b=20;
+},2000)
+
+console.log(a+b);
+
+// asynchronous method resolve
+
+
+let waitingData= new Promise((resolve, reject) => {
+  setTimeout(()=>{
+    resolve(30)
   })
-    console.log(result);
-    const fs=require('fs');
-    console.log("code step by step");
+})
 
-    fs.writeFileSync("hello2.txt","like and subscribe")
-    const http = require('http');
+  waitingData.then((data)=>{
+    console.log(a+data);
+  })
 
-    http.createServer((req,res)=>{
-        res.write("<h1>Hello, World</h1>");
-        res.end();
-    }).listen(4500);
+  //render html
+
+  const express =require('express');
+
+  const app=express();
+
+  app.get('',(req,res)=>{
+    res.send(`<h1>Welcome,This is Home Page</h>
+    
+        <a href="/about">Go to about page</a>
+    
+    `); 
+  }) 
+
+  app.get('/About',(req,res)=>{
+    res.send(`
+
+     <input type="text" placeholder="User Name"/>
+     <button>click me</button>
+    
+    `);
+  }) 
+
+
+  // make html page
 
 
 
+  
+  const publicpath=path.join(__dirname,'public');
+  app.use(express.static(publicpath));
+
+  // app.listen(5000);
